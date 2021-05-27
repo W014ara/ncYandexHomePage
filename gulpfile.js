d3f74a0b3dd81b21fac330736cf76523b75de398
W014ara <bezruk2000@mail.ru>
2021-05-28 01:27:12 +0300

    test heroku

---
+++ gulpfile.js
@@ -0,0 +1,92 @@
+const { series, parallel, src, dest } = require("gulp");
+
+const gulp = require("gulp"),
+  svgmin = require("gulp-svgmin"),
+  svgstore = require("gulp-svgstore"),
+  inject = require("gulp-inject"),
+  less = require("gulp-less"),
+  autoprefixer = require("gulp-autoprefixer"),
+  browserSync = require("browser-sync").create(),
+  rename = require("gulp-rename"),
+  modifyCssUrls = require('gulp-modify-css-urls');
+
+gulp.task("svgstore", function () {
+  const svgs = gulp
+    .src("./src/assets/icons/**/*.svg")
+    .pipe(
+      svgmin(function () {
+        return {
+          plugins: [
+            {
+              removeTitle: true,
+            },
+            {
+              removeStyleElement: true,
+            },
+          ],
+        };
+      })
+    )
+    .pipe(rename({ prefix: "icon-" }))
+    .pipe(svgstore({ inlineSvg: true }));
+
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src("./src/index.html")
+    .pipe(inject(svgs, { transform: fileContents }))
+    .pipe(gulp.dest("./src"));
+});
+
+
+gulp.task('fonts', function() {
+  return src('./src/assets/font/*')
+    .pipe(dest('dist/src/font'))
+})
+
+gulp.task('img', function() {
+  return src('./src/assets/img/*')
+    .pipe(dest('dist/src/img'))
+})
+
+gulp.task("less", function () {
+  return src("./src/assets/styles/main.less")
+    .pipe(less({
+      relativeUrls: false,
+      rebase: false
+    }))
+    .pipe(
+      autoprefixer({
+        cascade: false,
+      })
+    ).pipe(modifyCssUrls({
+      modify: function (url, filePath) {
+        return 'src/' + url.replace(/(\.\.\/)+/, '');
+      }
+    })).pipe(dest("./dist"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("./src/index.html").pipe(gulp.dest("./dist"));
+});
+
+gulp.task("serve", function () {
+  browserSync.init({
+    port:5051,
+    server: {
+      baseDir: "dist",
+    },
+  });
+
+  gulp.watch("./src/assets/styles/**/*.less").on("change", series("less"));
+  gulp.watch("./src/index.html").on("change", series("html"));
+
+  gulp.watch("./dist/style.css").on("change", browserSync.reload);
+  gulp.watch("./dist/index.html").on("change", browserSync.reload);
+});
+
+gulp.task("build", series("fonts", "img", "svgstore", "less","html"));
+
+gulp.task("default", series("svgstore", "fonts", "img", parallel("html", "less"), "serve"));
