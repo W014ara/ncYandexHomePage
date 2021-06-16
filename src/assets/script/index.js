cb20b1c26ecd685883d978f5bc1c7544daebfe4e
W014ara <bezruk2000@mail.ru>
2021-06-16 23:49:37 +0300

    finished Yandex Zen verst, added imitation of logic for adding cards

--- src/assets/script/index.js
+++ src/assets/script/index.js
@@ -1,3 +1,51 @@
+const data = [
+    {
+        source: "Яндекс.Новости",
+        sourceimg:"https://avatars.mds.yandex.net/get-zen-logos/246004/pub_5bc73bda17667700aaf44f9a_5edf8efe8d5c925baba78285/36x36_2x",
+        action: "",
+        baseurl: "https://avatars.mds.yandex.net/get-ynews/3293387/159f5a8eccdb6777c802aea5087cabf7/smart_crop_356x267",
+        title: "Windows 11 получит 6 пользовательских версий",
+        discription:"Масштабная утечка о Windows 11 раскрыла немало важных подробностей.",
+        publish: "день назад",
+        comment: true
+    },
+    {
+        source: "Знакомые лица",
+        sourceimg:"https://avatars.mds.yandex.net/get-zen-logos/212539/pub_5bcfdccab9fcb700aa296d9e_5fce6f050a45a91cf4622a8c/36x36_2x",
+        action: "",
+        baseurl: "https://avatars.mds.yandex.net/get-zen_doc/1590365/pub_60bb071de14854002ed94cf7_60bb071f0716567b35274915/smart_crop_356x267",
+        title: "Василия Шукшина не стало на съёмках «Они сражались за Родину»: кого снимали вместо него— да так, что никто не догадался о замене",
+        discription:"Фильм Сергея Бондарчука «Они сражались за Родину» стал последней творческой работой Василия Шукшина. Он не успел довести …",
+        publish: "сегодня",
+        comment: false
+    },
+    {
+        source: "Тинькофф",
+        sourceimg:"https://avatars.mds.yandex.net/get-zen-logos/1540393/pub_592d906ad7d0a6f37915023b_5ce50780e1ea8802c2e29bdb/36x36_2x",
+        action: "вы подписаны",
+        baseurl: "https://avatars.mds.yandex.net/get-zen_doc/4979934/pub_607ee4e2b1dee050509a9714_607ee772330a4f14d56df07f/smart_crop_356x267",
+        title: "Почему лучше взять кредитку, а не кредит?",
+        discription:"Выбирать между кредиткой и кредитом — все равно что выбирать между собственным автомобилем и самолетом. Из Москвы до…",
+        publish: "год назад",
+        comment: true
+    },
+    {
+        source: "Виктория Миронова",
+        sourceimg:"https://avatars.mds.yandex.net/get-zen-logos/200214/pub_60b7ae46b0886d640cd306f3_60b8b9e417777f0620759a00/36x36_2x",
+        action: "Личный блог",
+        baseurl: "https://avatars.mds.yandex.net/get-zen_doc/3927246/pub_60ca3665f7ddc664b1e0c4be_60ca3a016f4a071f4977e301/smart_crop_356x267",
+        title: "Это те самые сериалы, которые мы так долго ждали",
+        discription:"Вот уже мы стремительно подошли ко второй половине 2021 года, которая так и наполнена новыми сезонами наших…",
+        publish: "10 секунд назад",
+        comment: true
+    }
+]
+
+function getRandomArbitrary(min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+}
+
+
 const loginBlock = document.querySelector('.widgets__wrapper');
 const upperheaderBlock = document.querySelector('.header-topheader');
 const currentDay = document.querySelector('.title-geocurrenttime');
@@ -5,8 +53,6 @@ const searchInputInput = document.querySelector('#yasearch');
 const searthInput = document.querySelector('.container-yandexsearch');
 const searchInputBtn = document.querySelector('.container-yandexsearch__searchbtn');
 const dzen = document.querySelector(".dzen__table");
-const dzen__block = `<div class="dzen__item dzen__item-col2"></div>
-                    <div class="dzen__item dzen__item-col2 dzen__item-style2"></div>`
 
 const yandexYellowColor = "#fc0";
 const yandexYellowColorHighligh = "rgb(238, 191, 4)";
@@ -104,10 +150,151 @@ window.addEventListener('resize', (e) => {
 })
 
 window.addEventListener("scroll", (e)=>{
-    if(window.scrollY + document.body.offsetHeight >= document.body.scrollHeight){
-        setTimeout(() => {
-            dzen.insertAdjacentHTML("beforeend", dzen__block);
-        }, 500);
-    }
+    insertNewsBlock();
 })
 
+
+function validEmptyBlockAction(data, index){
+    if(data[index].action !== ""){
+        return `<p class="dzen__item-subscribe-info">${data[index].action}</p>`
+    }
+    return "";
+}
+
+function validEmptyCommentBlock(data, index){
+    console.log(data, index);
+    if(data[index].comment){
+        return `<a class="dzen__item-footer-comment" href="">
+        <div class="widget-avatar _auth _dzen"></div>
+        <p class="dzen__item-footer-text">Комментировать...</p>
+    </a>`
+    }
+    return `<a class="dzen__item-footer-comment _disabled" href="">
+    <div class="widget-avatar _auth _dzen"></div>
+    <p class="dzen__item-footer-text">Комментировать...</p>
+</a>`;
+}
+
+
+function insertNewsBlock(){
+    if(window.scrollY + document.body.offsetHeight >= document.body.scrollHeight){
+        let randomNewsLeftIndex = getRandomArbitrary(0, data.length);
+        let randomNewsRightIndex = getRandomArbitrary(0, data.length);
+        const leftBlock =
+        `<div class="dzen__item dzen__item-col2"><a href="" class="dzen__item-mainLink">
+        <div class="dzen__item-mainHeader">
+            <div class="container">
+                <div class="container__dzen-header">
+                    <img class="marker marker__dzen marker__dzen-main" width="36px" height="36px" src="${data[randomNewsLeftIndex].sourceimg}" alt="">
+                    <div class="dzen__item-subscribe">
+                        <p class="dzen__item-subscribe-text">${data[randomNewsLeftIndex].source}</p>
+                        ${validEmptyBlockAction(data, randomNewsLeftIndex)}
+                    </div>
+                </div>
+            </div>
+            <div class="dzen__item-functional">
+                <button class="marker-dzen__title" type="button">
+                    <svg class="marker-dzen__title-icon">
+                        <use href="#icon-dzen__subscribe"></use>
+                    </svg>
+                </button>
+                <button class="marker-dzen__title" type="button">
+                    <svg class="marker-dzen__title-icon _transperent">
+                        <use href="#icon-settings"></use>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <img class="dzen__item-splash" src="${data[randomNewsLeftIndex].baseurl}" alt="${data[randomNewsLeftIndex].source}">
+        <div class="dzen__item-maincontent">
+            <h1 class="title__dzen-card">${data[randomNewsLeftIndex].title}</h1>
+            <p class="dzen__item-maincontent-text">${data[randomNewsLeftIndex].discription}</p>
+            <span class="dzen__item-maincontent-time">${data[randomNewsLeftIndex].publish}</span>
+        </div>
+    </a>
+    <div class="dzen__item-footer">
+        ${validEmptyCommentBlock(data, randomNewsLeftIndex)}
+        <div class="dzen__item-footer-btn">
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen__share"></use>
+                </svg>
+            </button>
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen__like"></use>
+                </svg>
+            </button>
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen_dislike"></use>
+                </svg>
+            </button>
+        </div>
+    </div></div>`;
+        const rightBlock =
+        `<div class="dzen__item dzen__item-col2 dzen__item-style2"><a href="" class="dzen__item-mainLink">
+        <div class="dzen__item-mainHeader">
+            <div class="container">
+                <div class="container__dzen-header">
+                    <img class="marker marker__dzen marker__dzen-main" width="36px" height="36px" src="${data[randomNewsRightIndex].sourceimg}" alt="">
+                    <div class="dzen__item-subscribe">
+                        <p class="dzen__item-subscribe-text">${data[randomNewsRightIndex].source}</p>
+                        ${validEmptyBlockAction(data, randomNewsRightIndex)}
+                    </div>
+                </div>
+            </div>
+            <div class="dzen__item-functional">
+                <button class="marker-dzen__title" type="button">
+                    <svg class="marker-dzen__title-icon">
+                        <use href="#icon-dzen__subscribe"></use>
+                    </svg>
+                </button>
+                <button class="marker-dzen__title" type="button">
+                    <svg class="marker-dzen__title-icon _transperent">
+                        <use href="#icon-settings"></use>
+                    </svg>
+                </button>
+            </div>
+        </div>
+        <img class="dzen__item-splash _style2" src="${data[randomNewsRightIndex].baseurl}" alt="${data[randomNewsRightIndex].source}">
+        <div class="dzen__item-maincontent">
+            <h1 class="title__dzen-card">${data[randomNewsRightIndex].title}</h1>
+            <p class="dzen__item-maincontent-text">${data[randomNewsRightIndex].discription}</p>
+            <span class="dzen__item-maincontent-time">${data[randomNewsRightIndex].publish}</span>
+        </div>
+    </a>
+    <div class="dzen__item-footer">
+        ${validEmptyCommentBlock(data, randomNewsRightIndex)}
+        <div class="dzen__item-footer-btn">
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen__share"></use>
+                </svg>
+            </button>
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen__like"></use>
+                </svg>
+            </button>
+            <button class="marker-dzen__title" type="button">
+                <svg class="marker-dzen__title-icon">
+                    <use href="#icon-dzen_dislike"></use>
+                </svg>
+            </button>
+        </div>
+    </div></div>`;
+        const resultBlock = leftBlock + rightBlock;
+        dzen.insertAdjacentHTML("beforeend", resultBlock);
+    }
+}
+
+function debounce(f, ms) {
+    let isCooldown = false;
+    return function() {
+      if (isCooldown) return;
+      f.apply(this, arguments);
+      isCooldown = true;
+      setTimeout(() => isCooldown = false, ms);
+    };
+  }
